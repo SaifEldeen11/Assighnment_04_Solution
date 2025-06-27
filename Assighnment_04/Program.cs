43a354b3647875b2065a7e7ed1d39932e7c45e0e
SaifAhmed11 <saifeldeenahmed942@gmail.com>
2025-06-27 21:17:41 +0300

    Add project files.

---
+++ Assighnment_04/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assighnment_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            Console.WriteLine("enter a number ");
+            int num = int.Parse(Console.ReadLine());
+            if(num%3==0 && num%4==0)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+
+                Console.WriteLine("no");
+            }
+            #endregion
+        }
+    }
+}
