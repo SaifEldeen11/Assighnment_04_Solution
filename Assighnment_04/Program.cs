0178b24dbc5ad533e882c91c0d82d48bfa5b4372
SaifAhmed11 <saifeldeenahmed942@gmail.com>
2025-06-27 21:36:26 +0300

    Q3&Q4&Q5

--- Assighnment_04/Program.cs
+++ Assighnment_04/Program.cs
@@ -17,21 +17,77 @@
             //    Console.WriteLine("no");
             //}
             #endregion
+
             #region Q2
-            Console.WriteLine("enter a number (positive / negative )");
-            int number = Convert.ToInt32(Console.ReadLine());
-            if(number > 0)
-            {
-                Console.WriteLine("positive");
-            }
-            else if (number < 0)
-            {
-                Console.WriteLine("negative");
-            }
-            else
-            {
-                Console.WriteLine("zero");
-            }
+            //Console.WriteLine("enter a number (positive / negative )");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //if(number > 0)
+            //{
+            //    Console.WriteLine("positive");
+            //}
+            //else if (number < 0)
+            //{
+            //    Console.WriteLine("negative");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("zero");
+            //}
+            #endregion
+
+            #region Q3
+            //Console.WriteLine("enter 3 nums ");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //int num3 = Convert.ToInt32(Console.ReadLine());
+            //int max = num1;
+            //int min = num1;
+            //if (num2 > max)
+            //{
+            //    max = num2;
+            //}
+            //if (num3>max)
+            //{
+            //    max = num3;
+            //}
+
+            //if(num2 < min)
+            //{
+            //    min = num2;
+            //}
+            //if (num3 < min)
+            //{
+            //    min = num3;
+            //}
+
+            #endregion
+
+            #region Q4
+            //Console.WriteLine("enter a number ");
+            //int number =Convert.ToInt32(Console.ReadLine());
+
+            //if (number % 2 == 0)
+            //{
+            //    Console.WriteLine("even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("odd");
+            //}
+            #endregion
+
+            #region Q5
+            //Console.WriteLine("enter a char ");
+            //char ch = Convert.ToChar(Console.ReadLine());
+            //if (ch=='a'||ch=='o'||ch=='u'||ch=='e'||ch=='i')
+            //{
+            //    Console.WriteLine("vowel");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("consonant");
+            //}
             #endregion
         }
     }
