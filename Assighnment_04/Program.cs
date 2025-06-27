db72fd507a808bc66194a62d1b7d9c0d68233cd2
SaifAhmed11 <saifeldeenahmed942@gmail.com>
2025-06-27 21:21:48 +0300

    Q1&Q2

--- Assighnment_04/Program.cs
+++ Assighnment_04/Program.cs
@@ -5,16 +5,32 @@
         static void Main(string[] args)
         {
             #region Q1
-            Console.WriteLine("enter a number ");
-            int num = int.Parse(Console.ReadLine());
-            if(num%3==0 && num%4==0)
+            //Console.WriteLine("enter a number ");
+            //int num = int.Parse(Console.ReadLine());
+            //if(num%3==0 && num%4==0)
+            //{
+            //    Console.WriteLine("yes");
+            //}
+            //else
+            //{
+
+            //    Console.WriteLine("no");
+            //}
+            #endregion
+            #region Q2
+            Console.WriteLine("enter a number (positive / negative )");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if(number > 0)
             {
-                Console.WriteLine("yes");
+                Console.WriteLine("positive");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("negative");
             }
             else
             {
-
-                Console.WriteLine("no");
+                Console.WriteLine("zero");
             }
             #endregion
         }
